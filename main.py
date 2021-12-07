34cbc514595a2eb09ec8a0bd9fdac4546ab2e00b
Bruno <brunovolko@gmail.com>
2021-12-07 15:10:44 -0300

    Add verifications

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 neo4j_port = os.environ.get('NEO4J_PORT', 7687)
 
+#TODO siempre acordarse de pasar todo a mayuscula o minuscula
 
-
-neo4jClient = GraphDatabase.driver(f"neo4j://192.168.0.246/:{neo4j_port}")
+neo4jClient = GraphDatabase.driver(f"neo4j://127.0.0.1/:{neo4j_port}")
 
 # neo4jClient = GraphDatabase.driver(f"neo4j://localhost:{neo4j_port}")
 neo4jSession = neo4jClient.session()
@@ -30,11 +30,11 @@ def get_all_routes_between_two_airports(source_airport_code: str, destination_ai
     
     validate_airport_codes(source_airport_code, destination_airport_code, airport_code_type)
 
-    max_steps = 1
+    max_steps = 1 # TODO ver cual será el max_steps
 
     result  = neo4jSession.run(
      
-        neo_query(max_steps,source_airport_code,destination_airport_code, airport_code_type)
+        routes_with_scales_support(0, max_steps,source_airport_code,destination_airport_code, airport_code_type)
      
      )
      # iterate over the result set 
@@ -62,11 +62,15 @@ def get_all_routes_between_two_airports_with_scales(source_airport_code: str, de
     if(scales > 3):
         raise HTTPException(status_code=400, detail="Scales must be 3 or less")
 
-    result = neo4jSession.run(
-     
-        neo_query(scales,source_airport_code,destination_airport_code, airport_code_type)
-     
-     )
+    results = neo4jSession.run(
+        routes_with_scales_support(scales, scales,source_airport_code,destination_airport_code, airport_code_type)
+    )
+    
+    #Iterate over the result set
+    for result in results:
+        print(result)
+    
+    
 
 #query 1
 @app.get("/queries/shortest_route_in_scales")
@@ -84,7 +88,6 @@ def get_shorthest_route_between_two_airports(source_airport_code: str, destinati
     #TODO devolver bien la info
     return result.single()[0]
 
-
 @app.get("/queries/airports_reachable_from_airport")
 def get_airports_reachable_from_airport(source_airport_code: str, airport_code_type: str,scales:int): 
 
@@ -97,9 +100,11 @@ def get_airports_reachable_from_airport(source_airport_code: str, airport_code_t
     )
 
     #TODO iterar result
+
+def routes_with_scales_support (mins_scales, max_scales, source, destination, code_type):
+    scales_multiplier = f"{mins_scales}..{max_scales}" if mins_scales != max_scales else f"{mins_scales}"
     
+    from_code_condition = f"icao: \"{source}\"" if code_type == "ICAO" else f"iata: \"{source}\""
+    destination_code_condition = f"icao: \"{destination}\"" if code_type == "ICAO" else f"iata: \"{destination}\""
 
-
-def neo_query (scales, source, destination, code_type):
-    #TODO usar ICAO o IATA depende el codetype
-    return f"MATCH p=(n:Airport {{icao:\"{source}\"}}) -[:HAS_ROUTE_TO*0..{scales}]->(m:Airport{{icao:\"{destination}\"}}) WITH *,relationships(p) as r RETURN n,m,r,nodes(p)"
+    return f"MATCH p=(n:Airport {{{from_code_condition}}}) -[:HAS_ROUTE_TO*{scales_multiplier}]->(m:Airport{{{destination_code_condition}}}) WITH *,relationships(p) as r RETURN n,m,r,nodes(p)"
