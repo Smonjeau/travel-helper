f6b478de62f69c86debdfe137f0ccac9883b39b5
Santiago <smonjeau@itba.edu.ar>
2021-12-07 15:07:28 -0300

    feat: add query -> airports reacheable from source

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def read_root():
     return {"Hello": "World"}
 
 
+
+#query 3
 @app.get("/queries/all_routes")
 def get_all_routes_between_two_airports(source_airport_code: str, destination_airport_code: str, airport_code_type: str):
     
@@ -52,6 +54,7 @@ def get_all_routes_between_two_airports(source_airport_code: str, destination_ai
 
 
 
+
 @app.get("/queries/all_routes_with_scales")
 def get_all_routes_between_two_airports_with_scales(source_airport_code: str, destination_airport_code: str, airport_code_type: str, scales: int):
 
@@ -65,6 +68,7 @@ def get_all_routes_between_two_airports_with_scales(source_airport_code: str, de
      
      )
 
+#query 1
 @app.get("/queries/shortest_route_in_scales")
 def get_shorthest_route_between_two_airports(source_airport_code: str, destination_airport_code: str, airport_code_type: str):
 
@@ -79,6 +83,21 @@ def get_shorthest_route_between_two_airports(source_airport_code: str, destinati
      )
     #TODO devolver bien la info
     return result.single()[0]
+
+
+@app.get("/queries/airports_reachable_from_airport")
+def get_airports_reachable_from_airport(source_airport_code: str, airport_code_type: str,scales:int): 
+
+    validate_airport_codes(source_airport_code, source_airport_code, airport_code_type)
+
+    if(scales > 3):
+        raise HTTPException(status_code=400, detail="Scales must be 3 or less")
+    result = neo4jSession.run(
+        f"MATCH p=(n:Airport {{icao:\"{source_airport_code}\"}}) -[:HAS_ROUTE_TO*{scales}]->(m:Airport) WITH *,relationships(p) as r RETURN p"
+    )
+
+    #TODO iterar result
+    
 
 
 def neo_query (scales, source, destination, code_type):
