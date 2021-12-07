1ef1a1bc776b2259ef01c289d26a5f15d8fe0ea1
Francisco Quesada <fquesada@itba.edu.ar>
2021-12-07 14:54:54 -0300

    fix: changed import to import from file instead of mongo

--- importNeo.py
+++ importNeo.py
@@ -1,23 +1,27 @@
 import os
-from pymongo import MongoClient
+import csv
 from neo4j import GraphDatabase
 
 
 if __name__ == "__main__":
-    mongo_port = os.environ.get('MONGO_PORT', 27017)
     neo4j_port = os.environ.get('NEO4J_PORT', 7687)
 
 neo4jClient = GraphDatabase.driver(f"neo4j://localhost:{neo4j_port}")
-mongoClient = MongoClient(port=mongo_port)
 neo4jSession = neo4jClient.session()
 
-
-mongoDb = mongoClient['travel-helper']
-airports = mongoDb.airports.find({})
-routes = mongoDb.routes.find({})
-for airport in airports:
-    neo4jSession.run(
+print("Importing airports...")
+with open('data/airports_neo.csv', 'r') as airports:
+    reader =  csv.DictReader(airports)
+    transaction = neo4jSession.begin_transaction()
+    for airport in reader:
+        transaction.run(
         f"CREATE (a:Airport {{ iata: \"{airport.get('iata','null')}\", icao: \"{airport.get('icao','null')}\", airport_id: {airport['airport_id']} }})")
-for route in routes:
-    neo4jSession.run(
-        f"MATCH (a:Airport {{ airport_id: {route['source_airport_id']}}}), (b:Airport {{ airport_id: {route['destination_airport_id']}}}) CREATE (a)-[r:HAS_ROUTE_TO {{ distance: {route['distance']} }}]->(b)")
+    transaction.commit()
+print("Importing routes...")
+with open('data/routes_neo.csv', 'r') as routes:
+    reader = csv.DictReader(routes)
+    transaction = neo4jSession.begin_transaction()
+    for route in reader:
+        transaction.run(
+         f"MATCH (a:Airport {{ airport_id: {route['source_airport_id']}}}), (b:Airport {{ airport_id: {route['destination_airport_id']}}}) CREATE (a)-[r:HAS_ROUTE_TO {{ distance: {route['distance']} }}]->(b)")
+    transaction.commit()
