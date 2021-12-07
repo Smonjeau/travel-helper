2dd0ca556a25dae9c95776384e358605f85bf740
Francisco Quesada <fquesada@itba.edu.ar>
2021-12-07 14:54:32 -0300

    feat: added export mongo data for neo

---
+++ exportMongoForNeo.py
@@ -0,0 +1,25 @@
+import csv
+import pymongo
+import os
+
+if __name__ == "__main__":
+    mongo_port = os.environ.get('MONGO_PORT', 27017)
+
+mongoClient = pymongo.MongoClient(port=mongo_port)
+mongoDb = mongoClient['travel-helper']
+
+airports_out = csv.writer(open("data/airports_neo.csv","w"))
+airports_headers = ['iata','icao','airport_id']
+airports_out.writerow(airports_headers)
+airports = mongoDb.airports.find({})
+for airport in airports:
+    airports_out.writerow([airport['iata'],airport['icao'],airport['airport_id']])
+
+routes_out = csv.writer(open("data/routes_neo.csv","w"))
+routes_headers = ['source_airport_id', 'destination_airport_id', 'distance', 'airline_id']
+routes_out.writerow(routes_headers)
+routes = mongoDb.routes.find({})
+for route in routes:
+    routes_out.writerow([route['source_airport_id'], route['destination_airport_id'], route['distance'], route['airline_id']])
+
+
